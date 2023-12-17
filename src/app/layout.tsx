9080e9a10a453f1d3b03b4b51d44b5267e0f4ae1
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-12-17 21:05:43 +0100

    refactor: use `Outlet` in home layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
+import { Outlet } from "react-router";
+
 import Drawer from "./_components/drawer";
 
-export interface HomeLayoutProps extends React.PropsWithChildren {}
-
-export default function HomeLayout({ children }: HomeLayoutProps) {
+export default function HomeLayout() {
   return (
-    <>
+    <div className="flex">
       <Drawer />
-      <main>{children}</main>
-    </>
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
   );
 }
