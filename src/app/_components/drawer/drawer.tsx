5939d7192b272558b632d6646e5add9834202e1c
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2023-12-17 21:55:46 +0100

    bugfix: highlight current chat entry based on page route

--- src/app/_components/drawer/drawer.tsx
+++ src/app/_components/drawer/drawer.tsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router";
+import { useLoaderData, useParams } from "react-router";
 import { Link } from "react-router-dom";
 
 import { HomeLoader } from "@/app/loader";
@@ -8,15 +8,18 @@ import SearchInput from "./search-input";
 
 export default function Drawer() {
   const { rooms } = useLoaderData() as HomeLoader;
+  const { roomId } = useParams();
+
+  const isCurrent = (roomId_: string) => roomId_ === roomId;
 
   return (
     <aside className="w-72 px-4 py-2 border-r min-h-screen">
       <SearchInput placeholder="Search" />
 
       <div className="mt-4 space-y-0.5 overflow-y-auto">
-        {rooms.map((room, i) => (
+        {rooms.map((room) => (
           <Link key={room.roomId} to={`/rooms/${room.roomId}`}>
-            <ChatEntry isCurrent={i === 2} user={room.user} message={room.messages?.at(-1)} />
+            <ChatEntry isCurrent={isCurrent(room.roomId)} user={room.user} message={room.messages?.at(-1)} />
           </Link>
         ))}
       </div>
